b5c50aea8338d7745305676242f81ec922bb701b
AYehia0 <ahmedyehia611@gmail.com>
2021-01-18 00:11:01 +0200

    used a smarter way to scrape the table

--- scrapper.py
+++ scrapper.py
@@ -1,15 +1,21 @@
 import requests
+import json as js
 from bs4 import BeautifulSoup as bs
+
+# The autoCompletion link 
+# https://egybest.site/autoComplete.php?q=saw
 
 #faster way to scrap better t
 class Scrapper:
     
     def __init__(self, url):
-        self.srcape_url = url
+
+        self.scrape_url = url
         self.soup = self.init_soup()
 
     def init_soup(self):
-        self.r_url = requests.get(self.srcape_url).text
+
+        self.r_url = requests.get(self.scrape_url).text
         self.soup = bs(self.r_url, 'lxml')
 
         return self.soup
@@ -17,22 +23,38 @@ class Scrapper:
     #gets type, quality and size of a movie
     def get_movie_details(self):
         qualities_sizies = list()
-        api_links = list()
-        download_table = self.soup.find(class_='dls_table btns full mgb').tbody.find_all('tr')
+        download_table = self.soup.find(class_='dls_table btns full mgb')
 
         #5 is the number of all possible qualities, there is a better way to do it but i want to sleep now 
         try:
-            for i in range(5):
-                for td in download_table[i].find_all('td'):
-                    x = td.find('a', class_="nop btn g dl _open_window")
-                    if td.text != " تحميل  مشاهدة ":
-                        qualities_sizies.append(td.text)
-                    if x is not None:
-                        api_links.append(x.get('data-url'))
+            for tr in download_table.find_all('tr')[1:]:
+                tds = tr.find_all('td')
+                for td in tds[:3]:
+                    qualities_sizies.append(td.text)
+
         except Exception as e:
             print("Wrong page or still loading : " + e)
-        return qualities_sizies, api_links               
+        return qualities_sizies              
 
-
-
+    #searching using the autocomplete
+    # {
+    # "saw": [
+    #     {
+    #         "t": "Saw (2004)",
+    #         "u": "movie/saw-2004",
+    #         "i": "1984057894"
+    #     },
+    #     {
+    #         "t": "Saw II (2005)",
+    #         "u": "movie/saw-ii-2005",
+    #         "i": "1984064979"
+    #     },
+    #     {
+    #         "t": "Saw III (2006)",
+    #         "u": "movie/saw-iii-2006",
+    #         "i": "1984063562"
+    #     },
+    # ]
+    # }
+  
 
